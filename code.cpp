879a1630659d92c65bbda4c9330cdca8c7c78870
srikarreddy03 <93270470+srikarreddy03@users.noreply.github.com>
2024-02-19 10:50:55 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, world!" << std::endl;
     std::cout << "Hello, India!" << std::endl;
-    std::cout << "Hello, Hyderabad!" << std::endl;
+    std::cout << "I am Shreekar Reddi" << std::endl;
     return 0;
 } 
